f1cf692d6fff8f1667383c654114460c340b994f
Vilhelm Sturen <vilhelm.sturen@gmail.com>
2018-05-16 14:13:32 +0200

    no message

--- templateUITests/templateUITests.swift
+++ templateUITests/templateUITests.swift
@@ -28,9 +28,8 @@ class templateUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testPressTheRedButton() {
+        XCUIApplication().buttons["redButton"].tap()
     }
     
 }
