9d407096234dd70cd41123786ef1afaa6d426d37
Vilhelm Sturen <vilhelm.sturen@gmail.com>
2018-05-16 15:18:25 +0200

    Nimble quick tests

--- templateTests/templateTests.swift
+++ templateTests/templateTests.swift
@@ -7,30 +7,18 @@
 //
 
 import XCTest
+import Nimble
+import Quick
+
 @testable import template
 
-class templateTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class TestClass: QuickSpec {
+    override func spec() {
+        describe("A test for test") {
+            it("Passes when you need to") {
+                expect(1) == 1
+            }
+
         }
     }
-    
 }
